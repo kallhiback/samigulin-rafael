9189665b74f4beaffa456f876e01832eb82c6225
kallhiback <130337500+kallhiback@users.noreply.github.com>
2023-08-19 10:30:05 +0600

    Update lesson.js

--- js/lesson.js
+++ js/lesson.js
@@ -21,3 +21,49 @@ phoneCheck.onclick = () => {
 }
 
 
+// TAB SLIDER
+
+const tabContent = document.querySelectorAll('.tab_content_block')
+const tabs = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+let index = 0
+const hideTabContent = () => {
+    tabContent.forEach((item) => {
+        item.style.display = 'none'
+    })
+    tabs.forEach((item) => {
+        item.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (index = 0) => {
+    tabContent[index].style.display = 'block'
+    tabs[index].classList.add('tab_content_item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabs.forEach((item, i) => {
+            if (item === event.target){
+                hideTabContent()
+                showTabContent(i)
+            }
+        })
+    }
+}
+
+const Slider = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > tabContent.length - 1) {
+            i = 0
+        }
+        hideTabContent()
+        showTabContent(i)
+        }, 3000)
+}
+Slider(index)
+
